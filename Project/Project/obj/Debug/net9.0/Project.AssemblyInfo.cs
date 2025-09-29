7d70f04b9a99b126275a99d68aacdaa768fd11fb
Ahmad <ahib2004@gmail.com>
2025-09-29 21:45:59 +0300

    Controllers and Views Day 2 MVC Done

--- Project/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdb94e30d13c457affbfffabda399165edf6da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847212f1704c6ca71433e80c302f496937aa9d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
